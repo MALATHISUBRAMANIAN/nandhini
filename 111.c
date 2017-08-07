9f1e7c4b7155a2bac298d73de1c32f3193575a76
MALATHISUBRAMANIAN <30365873+MALATHISUBRAMANIAN@users.noreply.github.com>
2017-08-07 12:04:01 +0530

    Create 111.c

---
+++ 111.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+void main()
+{
+    int num;
+    printf("Enter any number: ");
+    scanf("%d", &n);
+     
+ 
+    if(n > 0)
+    {
+        printf("Number is POSITIVE");
+    }
+    else if(n < 0)
+    {
+        printf("Number is NEGATIVE");
+    }
+    else
+    {
+        printf("Number is ZERO");
+    }
+ 
+} 
