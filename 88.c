8cc87384abb9d78e175caf9aa63594fd7d696c75
MALATHISUBRAMANIAN <30365873+MALATHISUBRAMANIAN@users.noreply.github.com>
2017-08-07 11:54:23 +0530

    Create 88.c

---
+++ 88.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int a, i, sum = 0;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&a);
+
+    for(i=1; i <= a; ++i)
+    {
+        sum += i;   
+    }
+
+    printf("Sum = %d",sum);
+
+}
